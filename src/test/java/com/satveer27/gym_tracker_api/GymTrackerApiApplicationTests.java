db59bae251e6fc09147606cb188e272018e78058
Satveer27 <dabisama27@gmail.com>
2026-03-20 03:09:33 +0000

    Added the required files

---
+++ src/test/java/com/satveer27/gym_tracker_api/GymTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satveer27.gym_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
