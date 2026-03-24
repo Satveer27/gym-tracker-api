48662e051ec2db2ba15e39513fd51f67370f0309
Satveer27 <dabisama27@gmail.com>
2026-03-24 03:33:50 +0000

    chore: created user entity
    Created user entity file to reflect users schema in the database

---
+++ src/main/java/com/satveer27/gym_tracker_api/entity/User.java
@@ -0,0 +1,37 @@
+package com.satveer27.gym_tracker_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name= "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name="password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
